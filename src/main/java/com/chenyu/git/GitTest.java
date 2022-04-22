3fe1bbe32d1991c1b4a2f45a684620d7a2f9f5b6
USERchenyu <chneyu123163@126.com>
2022-04-22 14:20:29 +0800

    push test commit

--- src/main/java/com/chenyu/git/GitTest.java
+++ src/main/java/com/chenyu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello master test");
         System.out.println("hello hot-fix test");
+        System.out.println("push test");
     }
 }
